076ba72944462f57ecab91a69c1a6b478de7e957
Shalom-Solutions <omogoyejuwon@gmail.com>
2023-07-06 16:01:54 +0100

    created the database and configured the required models

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const {logger} = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const cookieParser = require('cookie-parser')
-const PORT =  process.env.PORT || 3500
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -15,12 +22,22 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/', require('./routes/root'));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
 
-
-app.all('*', require('./routes/404'));
-
+app.all("*", require("./routes/404"));
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`server running at ${PORT}`));
+
+mongoose.connection.once("open", () => {
+  console.log(`Connected to MongoDB`);
+  app.listen(PORT, () => console.log(`server running at ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog. log"
+  );
+});
